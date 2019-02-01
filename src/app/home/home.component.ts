e31dd6854af5100e3ddc8a7cfd7ea816dc0f3535
Léon <leon@boook.io>
2019-02-01 19:49:27 +0400

    Update homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,6 +13,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor(
     private userService: UserService
   ) { 
+    console.log('Homepage loaded...')
   }
 
   ngOnInit() {
